0900a419416b82f2b491d74ef3c790cbbd2394b2
huxiulin <1562914460@qq.com>
2020-03-19 16:29:31 +0800

    Mod:测试

--- app.js
+++ app.js
@@ -9,10 +9,7 @@ App({
     // 登录
     wx.login({
       success: res => {
-
-
-
-        console.log('11111111')
+        console.log('11111111'+'0000')
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
